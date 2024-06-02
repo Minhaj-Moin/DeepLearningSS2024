4922f95121df1fae2f6a5db0d37632c356a39e1f
Minhaj Ahmed Moin <minhaj.moin@outlook.com>
2024-06-02 20:42:09 +0200

    merged work

---
+++ exercise2_material/src_to_implement/Layers/ReLU.py
@@ -0,0 +1,14 @@
+from  Layers.Base  import BaseLayer
+import numpy as np
+class ReLU(BaseLayer):
+	
+	def __init__(self):
+		BaseLayer.__init__(self)
+		self.value = None
+	def forward(self, input_tensor):
+		self.value = np.abs((input_tensor > 0) * input_tensor)
+		return self.value
+		
+	def backward(self, error_tensor):
+		return error_tensor * np.abs((self.value > 0) * 1.)
+	
