868b70f767b8338ada6a3ee49460209474a7177e
Minhaj-Moin <minhaj.moin@outlook.com>
2024-07-01 07:58:35 +0200

    Regularizers Done

--- exercise3_material/src_to_implement/Layers/BatchNormalization.py
+++ exercise3_material/src_to_implement/Layers/BatchNormalization.py
@@ -7,14 +7,57 @@ import numpy as np
 
 class BatchNormalization(BaseLayer):
 
-    def __init__(self):
+    def __init__(self, channels):
         BaseLayer.__init__(self)
-        self.value = None
+        self.channels = channels
+        self.trainable = True
+        self.testing_phase = False
+        self.initialize()
+        self.test_mean = 0
+        self.test_var = 0
+    def initialize(self):
+        self.weights = np.ones((self.channels,))
+        self.bias = np.zeros((self.channels,))
 
     def forward(self, input_tensor):
-        exps = np.exp(input_tensor.T - np.max(input_tensor, axis=1)).T
-        self.value = (exps.T / np.sum(exps, axis=1)).T
-        return self.value
+        alpha = 0.8
+        X_hat = np.zeros(input_tensor.shape)
+        if not self.testing_phase:
+            self.input_tensor_shape = input_tensor.shape
+            # print(input_tensor.shape, input_tensor.mean(axis=1).shape)
+            # X_hat = np.zeros(input_tensor.shape)
+            # print(input_tensor.mean(axis=1, keepdims=True))
+            for b in range(input_tensor.shape[0]):
+                if len(input_tensor.shape) > 2:
+                    for c in range(input_tensor.shape[1]):
+                        self.test_mean = alpha * self.test_mean + (1-alpha) * input_tensor[b,c].flatten().mean(axis=0)
+                        self.test_var = alpha * self.test_var + (1-alpha) * input_tensor[b,c].flatten().var(axis=0)
+                        X_hat[b,c] = (input_tensor[b,c] - input_tensor[b,c].flatten().mean(dtype='float64'))/np.sqrt(input_tensor[b,c].flatten().var(dtype='float64') + 1e-12)
+                        # print(input_tensor[b,c], input_tensor[b,c].flatten().mean(dtype='float64'))
+                else:
+                    self.test_mean = alpha * self.test_mean + (1-alpha) * input_tensor[b].mean()
+                    self.test_var = alpha * self.test_var + (1-alpha) * input_tensor[b].var()
+                    X_hat[b] = (input_tensor[b] - input_tensor[b].flatten().mean(dtype='float64'))/np.sqrt(input_tensor[b].flatten().var(dtype='float64') + 1e-12)
+            if len(input_tensor.shape) == 2:
+                X_hat = (input_tensor - input_tensor.mean(axis=0, keepdims=True))/np.sqrt(input_tensor.var(axis=0, keepdims=True) + 1e-12)
+                return self.weights * X_hat + self.bias
+            return self.reformat(self.weights * self.reformat(X_hat) + self.bias)
+        else:
+            # X_hat = (input_tensor - self.test_mean)/np.sqrt(self.test_var + 1e-12)
+            if len(input_tensor.shape) == 2: return self.weights * ((input_tensor - self.test_mean)/np.sqrt(self.test_var + 1e-12)) + self.bias
+            for b in range(input_tensor.shape[0]):
+                for c in range(input_tensor.shape[1]):
+                    X_hat[b,c] = (input_tensor[b,c] - self.test_mean)/np.sqrt(self.test_var + 1e-12)
+            return self.reformat(self.weights * self.reformat(X_hat) + self.bias)
+
+
+    def reformat(self, tensor):
+        if len(tensor.shape) == 2:
+            b , h , m, n = self.input_tensor_shape
+            return tensor.reshape(b, m, n, h).transpose(0,3,1,2)
+        else:
+            b , h , m, n = tensor.shape
+            return tensor.reshape(b, h, m * n).transpose(0,2,1).reshape(b * m * n, h)
 
     def backward(self, error_tensor):
         return self.value * (
--- exercise3_material/src_to_implement/Layers/Dropout.py
+++ exercise3_material/src_to_implement/Layers/Dropout.py
@@ -7,16 +7,17 @@ import numpy as np
 
 class Dropout(BaseLayer):
 
-    def __init__(self):
+    def __init__(self, probability):
         BaseLayer.__init__(self)
-        self.value = None
+        self.probability = probability
+        self.mask_tensor = None
+        self.testing_phase = False
 
     def forward(self, input_tensor):
-        exps = np.exp(input_tensor.T - np.max(input_tensor, axis=1)).T
-        self.value = (exps.T / np.sum(exps, axis=1)).T
-        return self.value
+        self.mask_tensor = np.random.binomial(size=input_tensor.shape, n=1, p=self.probability)
+        if self.testing_phase: return input_tensor
+        return (input_tensor * self.mask_tensor) * 1/self.probability
 
     def backward(self, error_tensor):
-        return self.value * (
-            (error_tensor.T - np.multiply(self.value, error_tensor).sum(axis=1).T).T
-        )
+        if self.testing_phase: return error_tensor
+        return error_tensor * self.mask_tensor * 1/self.probability
