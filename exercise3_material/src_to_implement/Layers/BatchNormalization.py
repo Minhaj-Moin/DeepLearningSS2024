984c24fe290a05f041e5ea1788611b53c8f2d08a
Minhaj-Moin <minhaj.moin@outlook.com>
2024-07-03 13:58:42 +0200

    Regularizers Done

--- exercise3_material/src_to_implement/Layers/BatchNormalization.py
+++ exercise3_material/src_to_implement/Layers/BatchNormalization.py
@@ -15,12 +15,14 @@ class BatchNormalization(BaseLayer):
         self.initialize()
         self.test_mean = 0
         self.test_var = 0
+        self.X_hat = None
     def initialize(self):
         self.weights = np.ones((self.channels,))
         self.bias = np.zeros((self.channels,))
 
     def forward(self, input_tensor):
         alpha = 0.8
+        self.input_tensor = input_tensor
         X_hat = np.zeros(input_tensor.shape)
         if not self.testing_phase:
             self.input_tensor_shape = input_tensor.shape
@@ -43,7 +45,9 @@ class BatchNormalization(BaseLayer):
             if len(input_tensor.shape) == 2:
                 print(X_hat.mean(axis=0).shape, input_tensor.shape)
                 X_hat = (input_tensor - input_tensor.mean(axis=0, keepdims=True))/np.sqrt(input_tensor.var(axis=0, keepdims=True) + 1e-12)
+                self.X_hat = X_hat
                 return self.weights * X_hat + self.bias
+            self.X_hat = X_hat
             return self.reformat(self.weights * self.reformat(X_hat) + self.bias)
         else:
             # X_hat = (input_tensor - self.test_mean)/np.sqrt(self.test_var + 1e-12)
@@ -69,6 +73,22 @@ class BatchNormalization(BaseLayer):
             return tensor.reshape(b, h, m * n).transpose(0,2,1).reshape(b * m * n, h)
 
     def backward(self, error_tensor):
-        return self.value * (
-            (error_tensor.T - np.multiply(self.value, error_tensor).sum(axis=1).T).T
-        )
+        if len(error_tensor.shape) > 2:
+            error_tensor = self.reformat(error_tensor)
+            self.X_hat = self.reformat(self.X_hat)
+            self.input_tensor = self.reformat(self.input_tensor)
+        dW = (error_tensor * self.X_hat).sum(axis=0)
+        dB = error_tensor.sum(axis=0)
+        # self.weights -= dW
+        # self.bias -= dB
+        self.gradient_bias = dB
+        self.gradient_weights = dW
+        dX_hat = error_tensor * self.weights
+        dVar = (error_tensor * (self.input_tensor - self.input_tensor.mean(axis=0)) * (-1/2) * np.power(self.input_tensor.var(axis=0) + 1e-12, -3/2)).sum(axis=0)
+        # print(self.input_tensor.var(axis=1)self.weights.shape)
+        dMean = (dW * -1/np.sqrt(self.input_tensor.var(axis=0) + 1e-12)).sum(axis=0)
+        zz1 = (dX_hat * 1/np.sqrt(self.input_tensor.var(axis=0) + 1e-12))
+        zz2 = (dVar * 2 * (self.input_tensor - self.input_tensor.mean(axis=0))/self.input_tensor.shape[0])
+        zz3 = (dMean * 1/self.input_tensor.shape[0])
+        return zz1 + zz2 + zz3
+
