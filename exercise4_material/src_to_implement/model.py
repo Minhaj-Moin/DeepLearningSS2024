bca84bd3ca5feb45fe7c5cbed16d0e32967c8694
Minhaj-Moin <minhaj.moin@outlook.com>
2024-07-22 06:49:51 +0200

    Model.py done

--- exercise4_material/src_to_implement/model.py
+++ exercise4_material/src_to_implement/model.py
@@ -1,60 +1,62 @@
-from torch import nn
-from torchvision.models.optical_flow.raft import ResidualBlock
+import torch
 
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride = 1, downsample = None):
-        super(ResidualBlock, self).__init__()
-        self.conv1 = nn.Sequential(
-                        nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = 1),
-                        nn.BatchNorm2d(out_channels),
-                        nn.ReLU())
-        self.conv2 = nn.Sequential(
-                        nn.Conv2d(out_channels, out_channels, kernel_size = 3, stride = 1, padding = 1),
-                        nn.BatchNorm2d(out_channels))
-        self.downsample = downsample
-        self.relu = nn.ReLU()
-        self.out_channels = out_channels
 
+class ResNet(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv = torch.nn.Conv2d(3, 64, kernel_size=5, stride=2, padding=0, bias=False)
+        self.bn = torch.nn.BatchNorm2d(64)
+        self.relu = torch.nn.ReLU(inplace=True)
+        self.maxpool = torch.nn.MaxPool2d(3, 2)
+        self.resblock1 = ResBlock(64, 64, 1)
+        self.resblock2 = ResBlock(64, 128, 2)
+        self.resblock3 = ResBlock(128, 256, 2)
+        self.resblock4 = ResBlock(256, 512, 2)
+        self.avg_pool = torch.nn.AvgPool2d((1, 1))
+        self.fc = torch.nn.Linear(512, 2)
+        self.sigmoid = torch.nn.Sigmoid()
 
     def forward(self, x):
-        residual = x
-        out = self.conv1(x)
-        out = self.conv2(out)
-        if self.downsample:
-            residual = self.downsample(x)
-        out += residual
-        out = self.relu(out)
+        x = self.maxpool(self.relu(self.bn(self.conv(x))))
+        x = self.resblock1(x)
+        x = self.resblock2(x)
+        x = self.resblock3(x)
+        x = self.resblock4(x)
+
+        x = x.mean([2, 3])
+        x = self.fc(x)
+        out = self.sigmoid(x)
+
         return out
 
-class model(nn.Module):
-    def __init__(self):
-        super(model, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2)
-        self.batchnorm1 = nn.BatchNorm2d()
-        self.relu1 = nn.ReLU()
-        self.flatten = nn.Flatten()
-        self.globalavgpool = nn.AdaptiveAvgPool2d()
-        self.FC = nn.Linear(in_features=512, out_features=2)
-        self.sigmoid = nn.Sigmoid()
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2)
 
-        self.res1 = ResidualBlock(64, 64,1)
-        self.res2 = ResidualBlock(64,128,2)
-        self.res3 = ResidualBlock(128, 256,2)
-        self.res4 = ResidualBlock(256, 512,2)
+class ResBlock(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, stride):
+        super(ResBlock, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.stride = stride
+
+        self.res_block = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels, self.out_channels, 3, stride=self.stride, padding=1, bias=False),
+            torch.nn.BatchNorm2d(self.out_channels),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Conv2d(self.out_channels, self.out_channels, 3, padding=1, bias=False),
+            torch.nn.BatchNorm2d(self.out_channels)
+        )
+
+        self.relu = torch.nn.ReLU(inplace=True)
+        self.downsample = False
+        if self.stride != 1 or self.in_channels != self.out_channels:
+            self.downsample = True
+
+        self.conv1x1 = torch.nn.Conv2d(self.in_channels, self.out_channels, 1, stride=self.stride, padding=0,
+                                       bias=False)
+        self.bn2 = torch.nn.BatchNorm2d(self.out_channels)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.batchnorm1(x)
-        x = self.relu1(x)
-        x = self.maxpool(x)
-        x = self.res1(x)
-        x = self.res2(x)
-        x = self.res3(x)
-        x = self.res4(x)
-        x = self.globalavgpool(x)
-        x = self.flatten(x)
-        x = self.FC(x)
-        x = self.sigmoid(x)
-        return x
-
+        identity = x
+        if self.downsample: identity = self.bn2(self.conv1x1(x))
+        return self.relu(self.res_block(x) + identity)
